8a4827a4550e60ad5a7d330edacb99ce6e1bb1c4
Digchhen <digchhen2001@gmail.com>
2026-09-17 13:00:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -611,7 +611,7 @@ if st.session_state.logged_in:
 # 5. THE LOGIN SCREEN (FALLBACK ACCESS WITH SIGN-UP)
 # ==========================================================
 
-    elif st.session_state.app_stage == "landing":
+    if st.session_state.app_stage == "landing":
         st.title("🚀 Stop Guessing Your E-commerce Margins")
         st.subheader("Instantly calculate net profits from your store data logs without linking risky live API endpoints.")
         st.write(" ")
